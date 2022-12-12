aa1b778c1536d59dffcae0bd4bb4fd33a75a855a
Paul Justine Ardiente <111719175+kibyes123@users.noreply.github.com>
2022-12-12 23:19:54 +0800

    Add files via upload

---
+++ ArdienteFuzzyLogic/ArdienteFuzzyLogic/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ArdienteFuzzyLogic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.Chart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.Chart.Legends.Add(legend4);
+            this.Chart.Location = new System.Drawing.Point(43, 75);
+            this.Chart.Name = "Chart";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.Chart.Series.Add(series4);
+            this.Chart.Size = new System.Drawing.Size(419, 292);
+            this.Chart.TabIndex = 0;
+            this.Chart.Text = "Chart";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(483, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 190);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Initial Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Initial Angle";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Turn on";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(483, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Turn Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(483, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(483, 404);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(480, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Throttle Output";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Chart);
+            this.Name = "Form1";
+            this.Text = "Ardiente Automobile Cruise Control";
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ArdienteFuzzyLogic/ArdienteFuzzyLogic/Form1.cs
@@ -0,0 +1,218 @@
+﻿using DotFuzzy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ArdienteFuzzyLogic
+{
+    public partial class Form1 : Form
+    {
+        FuzzyEngine fe;
+        MembershipFunctionCollection speed, angle, throttle;
+        LinguisticVariable myspeed, myangle, mythrottle;
+        bool canExit = false;
+
+       
+
+        FuzzyRuleCollection myrules;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCharts();
+            setMembers();
+            setRules();
+            setFuzzyEngine();
+        }
+        
+
+        public void setMembers()
+        {
+            textBox3.Text = "0";
+            speed = new MembershipFunctionCollection();
+            speed.Add(new MembershipFunction("LOW", 0.0, 0.0, 45.0, 50.0));
+            speed.Add(new MembershipFunction("OK", 45.0, 50.0, 50.0, 55.0));
+            speed.Add(new MembershipFunction("HIGH", 50.0, 55.0, 100.0, 100.0));
+            myspeed = new LinguisticVariable("SPEED", speed);
+
+
+            angle = new MembershipFunctionCollection();
+            angle.Add(new MembershipFunction("DOWN", -10.0, -10.0, -5.0, 0.0));
+            angle.Add(new MembershipFunction("LEVEL", -5.0, 1.0, 1.0, 5.0));
+            angle.Add(new MembershipFunction("UP", 0.0, 5.0, 10.0, 10.0));
+            myangle = new LinguisticVariable("ANGLE", angle);
+
+            throttle = new MembershipFunctionCollection();
+            throttle.Add(new MembershipFunction("LOW", 0.0, 0.0, 2.0, 4.0));
+            throttle.Add(new MembershipFunction("LM", 2.0, 4.0, 4.0, 6.0));
+            throttle.Add(new MembershipFunction("MED", 4.0, 6.0, 6.0, 8.0));
+            throttle.Add(new MembershipFunction("HM", 6.0, 8.0, 8.0, 10.0));
+            throttle.Add(new MembershipFunction("HIGH", 8.0, 10.0, 10.0, 10.0));
+            mythrottle = new LinguisticVariable("THROTTLE", throttle);
+            
+
+
+
+        }
+
+        public void setRules()
+        {
+            myrules = new FuzzyRuleCollection();
+            myrules.Add(new FuzzyRule("IF (SPEED IS HIGH) AND (ANGLE IS UP) THEN THROTTLE IS LM"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS HIGH) AND (ANGLE IS LEVEL) THEN THROTTLE IS LM"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS HIGH) AND (ANGLE IS DOWN) THEN THROTTLE IS LOW"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS OK) AND (ANGLE IS UP) THEN THROTTLE IS HM"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS OK) AND (ANGLE IS LEVEL) THEN THROTTLE IS MED"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS OK) AND (ANGLE IS DOWN) THEN THROTTLE IS LM"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS LOW) AND (ANGLE IS UP) THEN THROTTLE IS HIGH"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS OK) AND (ANGLE IS LEVEL) THEN THROTTLE IS HM"));
+            myrules.Add(new FuzzyRule("IF (SPEED IS OK) AND (ANGLE IS DOWN) THEN THROTTLE IS HM"));
+        }
+        public void setFuzzyEngine()
+        {
+            fe = new FuzzyEngine();
+            fe.LinguisticVariableCollection.Add(myspeed);
+            fe.LinguisticVariableCollection.Add(myangle);
+            fe.LinguisticVariableCollection.Add(mythrottle);
+            fe.FuzzyRuleCollection = myrules;
+        }
+        public void InitializeCharts()
+        {
+           
+            // Sets properties for temperature chart
+            Chart.Series[0].Name = "Chart";
+            Chart.Series[0].Color = Color.Red;
+            Chart.Series[0].ChartType = SeriesChartType.Line;
+            Chart.Series[0].Points.AddY(0);
+            Chart.ChartAreas[0].AxisY.Minimum = 0;
+            Chart.ChartAreas[0].AxisY.Maximum = 60;
+        }
+            private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void defuziffyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setMembers();
+            setRules();
+            //setFuzzyEngine();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setMembers();
+            setRules();
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            canExit = true;
+            Chart.Series[0].Points.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            InitializeCharts();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            canExit = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                new Thread(() =>
+                {
+                    while (!canExit)
+                    {
+                        if (button1.Enabled) on();
+                        Thread.Sleep(500);
+                        
+                    }
+                }).Start();
+                
+            }
+            finally
+            {
+                canExit = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void fuziffyvalues()
+        {
+            myspeed.InputValue = (Convert.ToDouble(textBox1.Text));
+            myspeed.Fuzzify("LOW");
+            myangle.InputValue = (Convert.ToDouble(textBox2.Text));
+            myangle.Fuzzify("DOWN");
+            myangle.InputValue = (Convert.ToDouble(textBox3.Text));
+            myangle.Fuzzify("DOWN");
+
+        }
+
+        public void updateChart()
+        {
+            double throttle = Convert.ToDouble(textBox3.Text);
+            // Updates chart for temperature
+            Chart.Invoke((MethodInvoker)(() =>
+            {
+                if (Chart.Series[0].Points.Count > 25) Chart.Series[0].Points.RemoveAt(0);
+
+                Chart.Series[0].Points.AddY(throttle);
+                Chart.ChartAreas[0].AxisX.Minimum = Chart.Series[0].Points[0].XValue;
+                Chart.ChartAreas[0].AxisX.Maximum = 25;
+            }));
+        }
+            public void defuzzy()
+        {
+            fe.Consequent = "THROTTLE";
+            textBox3.Invoke((MethodInvoker)(() =>
+            {
+                textBox3.Text = "" + fe.Defuzzify();
+            }));
+            
+        }
+
+        public void computenewspeed()
+        {
+
+            double oldspeed = Convert.ToDouble(textBox1.Text);
+            double oldthrottle = Convert.ToDouble(textBox3.Text);
+            double oldangle = Convert.ToDouble(textBox2.Text);
+            double newspeed = ((1 - 0.1) * (oldspeed)) + (oldthrottle - (0.1 * oldangle));
+            textBox1.Invoke((MethodInvoker)(() =>
+            {
+                textBox1.Text = "" + newspeed;
+            }));
+        }
+
+        public void on()
+        {
+            fuziffyvalues();
+            defuzzy();
+            computenewspeed();
+            updateChart();
+        }
+    }
+}
